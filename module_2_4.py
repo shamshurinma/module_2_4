495451218acf82510981c4e0e561f917eccd2494
Mikhail Shamshurin <shamshurinma@gmail.com>
2024-12-03 12:26:03 +0400

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,19 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers:
+    is_prime = True
+    if i == 1:
+        continue
+    else:
+        for j in range(2, i-1):
+            if i % j ==0:
+                is_prime = False
+                break
+    if is_prime:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+print('Список:', numbers)
+print('Простые числа:', primes)
+print('Непростые числа:', not_primes)
